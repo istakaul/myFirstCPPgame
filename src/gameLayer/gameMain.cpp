663bb315a31daf74e438ba6f1326b2904d901d4d
istakaul <34567728+istakaul@users.noreply.github.com>
2026-08-08 13:22:09 +0300

    AssetManager and assets

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -3,14 +3,18 @@
 #include <iostream>
 #include <fstream>
 #include <asserts.h>
+#include <assetManager.h>
 
 struct GameData {
-	float positionX = 100;
-	float positionY = 100;
+
 } gameData;
+
+AssetManager assetManager;
 
 bool initGame()
 {
+	assetManager.loadAll();
+
 	return true;
 }
 
@@ -30,12 +34,7 @@ TODO: delete the following region
 	float deltaTime = GetFrameTime();
 	if (deltaTime > 1.f / 5) { deltaTime = 1.f / 5; } // limit deltaTime to a maximum of 1/5 seconds (200ms) to avoid large jumps
 
-	if (IsKeyDown(KEY_A)) { gameData.positionX -= 200 * deltaTime; }
-	if (IsKeyDown(KEY_D)) { gameData.positionX += 200 * deltaTime; }
-	if (IsKeyDown(KEY_W)) { gameData.positionY -= 200 * deltaTime; }
-	if (IsKeyDown(KEY_S)) { gameData.positionY += 200 * deltaTime; }
-
-	DrawRectangle(gameData.positionX, gameData.positionY, 50, 50, {255, 0, 200, 255});
+	DrawTexturePro(assetManager.dirt, { 0,0,(float)assetManager.dirt.width, (float)assetManager.dirt.height }, { 50,50, 100, 100 }, { 0,0 }, 0, WHITE);
 
 	return true;
 }
