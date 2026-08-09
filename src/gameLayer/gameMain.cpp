63691d70e715c844b50fa515a5537b2aec329d8f
istakaul <34567728+istakaul@users.noreply.github.com>
2026-08-09 11:22:43 +0300

    Texture Atlas

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -5,9 +5,11 @@
 #include <asserts.h>
 #include <assetManager.h>
 #include <gameMap.h>
+#include <helpers.h>
 
 struct GameData {
 	GameMap gameMap;
+	Camera2D camera;
 } gameData;
 
 AssetManager assetManager;
@@ -19,10 +21,14 @@ bool initGame()
 	gameData.gameMap.create(30, 10);
 
 	gameData.gameMap.getBlockUnsafe(0, 0).type = Block::dirt;
-	gameData.gameMap.getBlockUnsafe(1, 1).type = Block::dirt;
-	gameData.gameMap.getBlockUnsafe(2, 2).type = Block::dirt;
-	gameData.gameMap.getBlockUnsafe(3, 3).type = Block::dirt;
-	gameData.gameMap.getBlockUnsafe(4, 4).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(1, 1).type = Block::grass;
+	gameData.gameMap.getBlockUnsafe(2, 2).type = Block::goldBlock;
+	gameData.gameMap.getBlockUnsafe(3, 3).type = Block::glass;
+	gameData.gameMap.getBlockUnsafe(4, 4).type = Block::platform;
+
+	gameData.camera.target = { 0.f, 0.f }; // world-space center of view
+	gameData.camera.rotation = 0.f; // no rotation
+	gameData.camera.zoom = 100.f; // no zoom
 
 	return true;
 }
@@ -32,7 +38,20 @@ bool updateGame()
 	float deltaTime = GetFrameTime();
 	if (deltaTime > 1.f / 5) { deltaTime = 1.f / 5; } // limit deltaTime to a maximum of 1/5 seconds (200ms) to avoid large jumps
 
+	gameData.camera.offset = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f }; // center of the screen
+
 	ClearBackground({ 75, 75, 150, 255 }); // clear the screen with a dark green color
+
+#pragma region camera movement
+
+	if(IsKeyDown(KEY_LEFT)) gameData.camera.target.x -= 7.f * deltaTime;
+	if(IsKeyDown(KEY_RIGHT)) gameData.camera.target.x += 7.f * deltaTime;
+	if(IsKeyDown(KEY_UP)) gameData.camera.target.y -= 7.f * deltaTime;
+	if(IsKeyDown(KEY_DOWN)) gameData.camera.target.y += 7.f * deltaTime;
+
+#pragma endregion
+
+	BeginMode2D(gameData.camera);
 	
 	for (int y = 0; y < gameData.gameMap.h; y++) {
 		for (int x = 0; x < gameData.gameMap.w; x++) {
@@ -40,14 +59,11 @@ bool updateGame()
 			auto &b = gameData.gameMap.getBlockUnsafe(x, y);
 
 			if (b.type != Block::air) {
-				float size = 32;
-				float posX = x * size;
-				float posY = y * size;
 
 				DrawTexturePro(
-					assetManager.dirt,
-					Rectangle{ 0, 0, (float)assetManager.dirt.width, (float)assetManager.dirt.height }, // source
-					{ posX, posY, size, size }, // destination
+					assetManager.textures,
+					getTextureAtlas(b.type, 0, 32, 32), // source
+					{ (float)x, (float)y, 1, 1}, // destination
 					{ 0, 0 }, // origin (top-left corner)
 					0.f, // rotation
 					WHITE // tint
@@ -55,6 +71,8 @@ bool updateGame()
 			}
 		}
 	}
+
+	EndMode2D();
 
 	return true;
 }
